93c1df3d1e377a9ec3eb783e62f9b889e6720c07
PIYUSH-ai-web <piyushyadav200666@gmail.com>
2024-12-04 16:02:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,23 @@ document.addEventListener("DOMContentLoaded", function() {
         coll[i].addEventListener("click", function() {
             this.classList.toggle("active");
             var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "block";
-            }
+            content.style.display = (content.style.display === "block") ? "none" : "block";
         });
     }
 });
 
 function copyText(textBoxId, button) {
     var textBox = document.getElementById(textBoxId);
-    textBox.select();
-    textBox.setSelectionRange(0, 99999); // For mobile devices
-    document.execCommand("copy");
-    button.innerText = "Copied!";
-    setTimeout(function() {
-        button.innerText = "Copy";
-    }, 2000);
+    
+    // Use the Clipboard API
+    navigator.clipboard.writeText(textBox.value)
+        .then(function() {
+            button.innerText = "Copied!";
+            setTimeout(function() {
+                button.innerText = "Copy";
+            }, 2000);
+        })
+        .catch(function(err) {
+            console.error('Could not copy text: ', err);
+        });
 }
